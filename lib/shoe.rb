a407ecd7d48544245cbea671011fbd539db2e7bf
Huy Do <huyddo40@gmail.com>
2019-06-13 16:12:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,13 @@
 class Shoe
 
-  attr_accessor :color, :size, :material, :condition, :cobble
+  attr_accessor :color, :size, :material, :condition, 
   attr_reader :brand
   def initialize(name)
     @name = name
   end
 
-  puts "Your shoe is as good as new!"
-
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
 
 end
