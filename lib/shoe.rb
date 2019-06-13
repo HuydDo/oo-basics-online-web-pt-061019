f0b57ef7b8958c8a8bc65462537d1262dfde8492
Huy Do <huyddo40@gmail.com>
2019-06-13 17:01:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
 
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material
   # attr_reader :brand
 
   def initialize(name)
@@ -16,9 +16,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  # def condition(condition)
-  #   @condition = condition
-  # end
+  def condition(condition)
+    @condition = condition
+  end
 
 end
 
