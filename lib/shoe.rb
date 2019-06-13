f0dbfba088ca20526e5b7b707e8997b9265c0b81
Huy Do <huyddo40@gmail.com>
2019-06-13 17:11:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
 
-  attr_accessor :color, :size, :material, :brand :condition
+  attr_accessor :color, :size, :material, :brand, :condition
   # attr_reader :brand
 
   def initialize(name)
